61779d3e917bca6a85e178ab19a380d79805d6f0
Radek Stankiewicz <radoslaws@google.com>
2025-04-15 16:15:04 +0200

    redistribute and reshuffle - packing and unpacking tracing context

--- runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/RedistributeByKeyOverrideFactory.java
+++ runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/RedistributeByKeyOverrideFactory.java
@@ -17,6 +17,8 @@
  */
 package org.apache.beam.runners.dataflow;
 
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import java.util.Collections;
 import org.apache.beam.runners.dataflow.internal.DataflowGroupByKey;
 import org.apache.beam.sdk.runners.AppliedPTransform;
@@ -47,92 +49,96 @@ class RedistributeByKeyOverrideFactory<K, V>
     extends SingleInputOutputOverrideFactory<
         PCollection<KV<K, V>>, PCollection<KV<K, V>>, RedistributeByKey<K, V>> {
 
-  @Override
-  public PTransformReplacement<PCollection<KV<K, V>>, PCollection<KV<K, V>>>
-      getReplacementTransform(
-          AppliedPTransform<PCollection<KV<K, V>>, PCollection<KV<K, V>>, RedistributeByKey<K, V>>
-              transform) {
-    return PTransformOverrideFactory.PTransformReplacement.of(
-        PTransformReplacements.getSingletonMainInput(transform),
-        new DataflowRedistributeByKey<>(transform.getTransform()));
-  }
-
-  /** Specialized implementation of {@link RedistributeByKey} for Dataflow pipelines. */
-  private static class DataflowRedistributeByKey<K, V>
-      extends PTransform<PCollection<KV<K, V>>, PCollection<KV<K, V>>> {
-
-    private final RedistributeByKey<K, V> originalTransform;
-
-    private DataflowRedistributeByKey(RedistributeByKey<K, V> originalTransform) {
-      this.originalTransform = originalTransform;
-    }
-
     @Override
-    public PCollection<KV<K, V>> expand(PCollection<KV<K, V>> input) {
-      WindowingStrategy<?, ?> originalStrategy = input.getWindowingStrategy();
-      Window<KV<K, V>> rewindow =
-          Window.<KV<K, V>>into(
-                  new IdentityWindowFn<>(originalStrategy.getWindowFn().windowCoder()))
-              .triggering(new ReshuffleTrigger<>())
-              .discardingFiredPanes()
-              .withTimestampCombiner(TimestampCombiner.EARLIEST)
-              .withAllowedLateness(Duration.millis(BoundedWindow.TIMESTAMP_MAX_VALUE.getMillis()));
-
-      PCollection<KV<K, ValueInSingleWindow<V>>> reified =
-          input
-              .apply("SetIdentityWindow", rewindow)
-              .apply("ReifyOriginalMetadata", Reify.windowsInValue());
-
-      PCollection<KV<K, Iterable<ValueInSingleWindow<V>>>> grouped;
-      if (originalTransform.getAllowDuplicates()) {
-        grouped = reified.apply(DataflowGroupByKey.createWithAllowDuplicates());
-      } else {
-        grouped = reified.apply(DataflowGroupByKey.create());
-      }
-
-      return grouped
-          .apply(
-              "ExpandIterable",
-              ParDo.of(
-                  new DoFn<
-                      KV<K, Iterable<ValueInSingleWindow<V>>>, KV<K, ValueInSingleWindow<V>>>() {
-                    @ProcessElement
-                    public void processElement(
-                        @Element KV<K, Iterable<ValueInSingleWindow<V>>> element,
-                        OutputReceiver<KV<K, ValueInSingleWindow<V>>> r) {
-                      K key = element.getKey();
-                      for (ValueInSingleWindow<V> value : element.getValue()) {
-                        r.output(KV.of(key, value));
-                      }
-                    }
-                  }))
-          .apply("RestoreMetadata", new RestoreMetadata<>())
-          .setWindowingStrategyInternal(originalStrategy);
+    public PTransformReplacement<PCollection<KV<K, V>>, PCollection<KV<K, V>>>
+    getReplacementTransform(
+            AppliedPTransform<PCollection<KV<K, V>>, PCollection<KV<K, V>>, RedistributeByKey<K, V>>
+                    transform) {
+        return PTransformOverrideFactory.PTransformReplacement.of(
+                PTransformReplacements.getSingletonMainInput(transform),
+                new DataflowRedistributeByKey<>(transform.getTransform()));
     }
-  }
 
-  private static class RestoreMetadata<K, V>
-      extends PTransform<PCollection<KV<K, ValueInSingleWindow<V>>>, PCollection<KV<K, V>>> {
-    @Override
-    public PCollection<KV<K, V>> expand(PCollection<KV<K, ValueInSingleWindow<V>>> input) {
-      return input.apply(
-          ParDo.of(
-              new DoFn<KV<K, ValueInSingleWindow<V>>, KV<K, V>>() {
-                @Override
-                public Duration getAllowedTimestampSkew() {
-                  return Duration.millis(Long.MAX_VALUE);
-                }
+    /**
+     * Specialized implementation of {@link RedistributeByKey} for Dataflow pipelines.
+     */
+    private static class DataflowRedistributeByKey<K, V>
+            extends PTransform<PCollection<KV<K, V>>, PCollection<KV<K, V>>> {
 
-                @ProcessElement
-                public void processElement(
-                    @Element KV<K, ValueInSingleWindow<V>> kv, OutputReceiver<KV<K, V>> r) {
-                  r.outputWindowedValue(
-                      KV.of(kv.getKey(), kv.getValue().getValue()),
-                      kv.getValue().getTimestamp(),
-                      Collections.singleton(kv.getValue().getWindow()),
-                      kv.getValue().getPane());
-                }
-              }));
+        private final RedistributeByKey<K, V> originalTransform;
+
+        private DataflowRedistributeByKey(RedistributeByKey<K, V> originalTransform) {
+            this.originalTransform = originalTransform;
+        }
+
+        @Override
+        public PCollection<KV<K, V>> expand(PCollection<KV<K, V>> input) {
+            WindowingStrategy<?, ?> originalStrategy = input.getWindowingStrategy();
+            Window<KV<K, V>> rewindow =
+                    Window.<KV<K, V>>into(
+                                    new IdentityWindowFn<>(originalStrategy.getWindowFn().windowCoder()))
+                            .triggering(new ReshuffleTrigger<>())
+                            .discardingFiredPanes()
+                            .withTimestampCombiner(TimestampCombiner.EARLIEST)
+                            .withAllowedLateness(Duration.millis(BoundedWindow.TIMESTAMP_MAX_VALUE.getMillis()));
+
+            PCollection<KV<K, ValueInSingleWindow<V>>> reified =
+                    input
+                            .apply("SetIdentityWindow", rewindow)
+                            .apply("ReifyOriginalMetadata", Reify.windowsInValue());
+
+            PCollection<KV<K, Iterable<ValueInSingleWindow<V>>>> grouped;
+            if (originalTransform.getAllowDuplicates()) {
+                grouped = reified.apply(DataflowGroupByKey.createWithAllowDuplicates());
+            } else {
+                grouped = reified.apply(DataflowGroupByKey.create());
+            }
+
+            return grouped
+                    .apply(
+                            "ExpandIterable",
+                            ParDo.of(
+                                    new DoFn<
+                                            KV<K, Iterable<ValueInSingleWindow<V>>>, KV<K, ValueInSingleWindow<V>>>() {
+                                        @ProcessElement
+                                        public void processElement(
+                                                @Element KV<K, Iterable<ValueInSingleWindow<V>>> element,
+                                                OutputReceiver<KV<K, ValueInSingleWindow<V>>> r) {
+                                            K key = element.getKey();
+                                            for (ValueInSingleWindow<V> value : element.getValue()) {
+                                                r.output(KV.of(key, value));
+                                            }
+                                        }
+                                    }))
+                    .apply("RestoreMetadata", new RestoreMetadata<>())
+                    .setWindowingStrategyInternal(originalStrategy);
+        }
     }
-  }
+
+    private static class RestoreMetadata<K, V>
+            extends PTransform<PCollection<KV<K, ValueInSingleWindow<V>>>, PCollection<KV<K, V>>> {
+        @Override
+        public PCollection<KV<K, V>> expand(PCollection<KV<K, ValueInSingleWindow<V>>> input) {
+            return input.apply(
+                    ParDo.of(
+                            new DoFn<KV<K, ValueInSingleWindow<V>>, KV<K, V>>() {
+                                @Override
+                                public Duration getAllowedTimestampSkew() {
+                                    return Duration.millis(Long.MAX_VALUE);
+                                }
+
+                                @ProcessElement
+                                public void processElement(
+                                        @Element KV<K, ValueInSingleWindow<V>> kv, OutputReceiver<KV<K, V>> r) {
+                                    r.outputWindowedValue(
+                                            KV.of(kv.getKey(), kv.getValue().getValue()),
+                                            kv.getValue().getTimestamp(),
+                                            Collections.singleton(kv.getValue().getWindow()),
+                                            kv.getValue().getPane(),
+                                            kv.getValue().getTracingContext());
+                                }
+
+                            }));
+        }
+    }
 }
