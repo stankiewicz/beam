528aafc334145d7ff71eeddf03cdd41a3d0c3246
Radek Stankiewicz <radoslaws@google.com>
2026-03-06 20:47:14 +0100

    remove warn

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1606,7 +1606,6 @@ class BeamModulePlugin implements Plugin<Project> {
           disabledChecks.each {
             options.errorprone.errorproneArgs.add("-Xep:${it}:OFF")
           }
-          options.errorprone.errorproneArgs.add("-Xep:AutoValueBoxedValues:WARN")
         }
       }
 
